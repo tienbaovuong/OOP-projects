ef3513d8766fa0e1c30e16a82255d4670f74aa15
Hoàng Anh <82218114+hoanganhak53@users.noreply.github.com>
2021-04-29 22:41:40 +0700

    update map 0 lan 2

--- java-rpg-master/DoorEvent.java
+++
@@ -1,9 +0,0 @@
-public class DoorEvent extends Event {
-    public DoorEvent(int x, int y) {
-        super(x, y, 18, true);
-    }
-
-    public String toString() {
-        return "DOOR:" + super.toString();
-    }
-}
